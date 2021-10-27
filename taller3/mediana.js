4b6cc29e8d5efc21dd03a70a7b5f81c3bcd31ae8
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-10-27 16:06:54 -0500

    Taller 3: Agregada la funcion calcularMediana() 2

--- taller3/mediana.js
+++ taller3/mediana.js
@@ -1,10 +1,10 @@
-const lista1 = [300, 100, 200, 400000000, 700, 80, 60, 30];
-/* const listaOrdenada = lista1.sort(function (a, b) {
+/*const lista1 = [300, 100, 200, 400000000, 700, 80, 60, 30];
+ const listaOrdenada = lista1.sort(function (a, b) {
   return a - b;
-});*/
+});
 // 30,60,80,100,200,300, 700, 400000000
 const mitadLista1 = parseInt(lista1.length / 2);
-
+*/
 function calcularMediaAritmetica(lista) {
   const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
     return valorAcumulado + nuevoElemento;
@@ -20,18 +20,6 @@ function esPar(numero) {
     return false;
   }
 }
-/*let mediana;
-if (esPar(lista1.length)) {
-  const elemento1 = listaOrdenada[mitadLista1 - 1];
-  const elemento2 = listaOrdenada[mitadLista1];
-
-  const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
-  mediana = promedioElemento1y2;
-} else {
-}
-*/
-//console.log("mediana= " + mediana);
-
 function calcularMediana(lista = []) {
   let listaOrdenada1 = lista.sort(function (a, b) {
     return a - b;
