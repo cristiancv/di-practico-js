f00a993cb207427544205c8680f680c4be05c8a3
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-10-27 16:39:08 -0500

    Taller 3 creada función calcularModa()

--- taller3/moda.js
+++ taller3/moda.js
@@ -1,6 +1,7 @@
 const lista1 = [1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1];
-const lista1Count = {};
+//const lista1Count = {};
 
+/*
 lista1.map(function (elemento) {
   if (lista1Count[elemento]) {
     lista1Count[elemento] += 1;
@@ -8,6 +9,8 @@ lista1.map(function (elemento) {
     lista1Count[elemento] = 1;
   }
 });
+*/
+/*
 const lista1Array = Object.entries(lista1Count).sort(function (
   elementoA,
   elementoB
@@ -15,3 +18,22 @@ const lista1Array = Object.entries(lista1Count).sort(function (
   return elementoA[1] - elementoB[1];
 });
 const moda = lista1Array[lista1Array.length - 1];
+*/
+const calcularModa = (lista = []) => {
+  let listaCount = {};
+  lista.map(function (elemento) {
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  });
+  const lista1Array = Object.entries(lista1Count).sort(function (
+    elementoA,
+    elementoB
+  ) {
+    return elementoA[1] - elementoB[1];
+  });
+  const moda = lista1Array[lista1Array.length - 1];
+  return moda;
+};
