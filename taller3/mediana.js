f586d267267a7bead1531a58fd77edd4c6f3bc34
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-10-27 16:04:49 -0500

    Taller 3: Agregada la funcion calcularMediana()

--- taller3/mediana.js
+++ taller3/mediana.js
@@ -1,7 +1,8 @@
 const lista1 = [300, 100, 200, 400000000, 700, 80, 60, 30];
-const listaOrdenada = lista1.sort(function (a, b) {
+/* const listaOrdenada = lista1.sort(function (a, b) {
   return a - b;
-});
+});*/
+// 30,60,80,100,200,300, 700, 400000000
 const mitadLista1 = parseInt(lista1.length / 2);
 
 function calcularMediaAritmetica(lista) {
@@ -19,7 +20,7 @@ function esPar(numero) {
     return false;
   }
 }
-let mediana;
+/*let mediana;
 if (esPar(lista1.length)) {
   const elemento1 = listaOrdenada[mitadLista1 - 1];
   const elemento2 = listaOrdenada[mitadLista1];
@@ -28,4 +29,22 @@ if (esPar(lista1.length)) {
   mediana = promedioElemento1y2;
 } else {
 }
-console.log("mediana= " + mediana);
+*/
+//console.log("mediana= " + mediana);
+
+function calcularMediana(lista = []) {
+  let listaOrdenada1 = lista.sort(function (a, b) {
+    return a - b;
+  });
+  let mitadLista = parseInt(listaOrdenada1.length / 2);
+  let mediana = 0;
+  if (esPar(listaOrdenada1.length)) {
+    const elemento1 = listaOrdenada1[mitadLista - 1];
+    const elemento2 = listaOrdenada1[mitadLista];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    mediana = promedioElemento1y2;
+    return `La mediana es: ${mediana}`;
+  } else {
+  }
+}
