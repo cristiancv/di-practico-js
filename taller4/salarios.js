3e6c1aebd77eda4e43e0763f2c85cdeffba46bb2
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-10-28 12:36:00 -0500

    taller 4 hasta secciòn mediana 1

---
+++ taller4/salarios.js
@@ -0,0 +1,101 @@
+const ecuador = [];
+//1
+ecuador.push({
+  name: "Camila",
+  salary: 500,
+});
+//2
+ecuador.push({
+  name: "Nath",
+  salary: 1500,
+});
+//3
+ecuador.push({
+  name: "Luisa",
+  salary: 700,
+});
+//4
+ecuador.push({
+  name: "Julia",
+  salary: 800,
+});
+//5
+ecuador.push({
+  name: "Maria",
+  salary: 750,
+});
+//6
+ecuador.push({
+  name: "Sandra",
+  salary: 1200,
+});
+//7
+ecuador.push({
+  name: "Juliana",
+  salary: 900,
+});
+//8
+ecuador.push({
+  name: "Luis",
+  salary: 800,
+});
+//9
+ecuador.push({
+  name: "Juan",
+  salary: 600,
+});
+//10
+ecuador.push({
+  name: "Jose",
+  salary: 850,
+});
+//11
+ecuador.push({
+  name: "Juan",
+  salary: 800,
+});
+//12
+ecuador.push({
+  name: "Andres",
+  salary: 1000,
+});
+//13
+ecuador.push({
+  name: "Dario",
+  salary: 600,
+});
+//14
+ecuador.push({
+  name: "Miguel",
+  salary: 1100,
+});
+//15
+ecuador.push({
+  name: "Carlos",
+  salary: 1000,
+});
+//16
+ecuador.push({
+  name: "Julio",
+  salary: 900,
+});
+//17
+ecuador.push({
+  name: "Angel",
+  salary: 1100,
+});
+//18
+ecuador.push({
+  name: "Isabel",
+  salary: 700,
+});
+//19
+ecuador.push({
+  name: "Walter",
+  salary: 800,
+});
+//20
+ecuador.push({
+  name: "Marco",
+  salary: 1100,
+});
