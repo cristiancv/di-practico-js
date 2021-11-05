d2131a4dc1cfc599167daeb3e6ce07ab74132ce1
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-11-05 11:31:02 -0500

    Mediana finalizada 1, y cambios menores a promedio

--- taller3/mediana.js
+++ taller3/mediana.js
@@ -1,10 +1,8 @@
-/*const lista1 = [300, 100, 200, 400000000, 700, 80, 60, 30];
- const listaOrdenada = lista1.sort(function (a, b) {
-  return a - b;
-});
-// 30,60,80,100,200,300, 700, 400000000
-const mitadLista1 = parseInt(lista1.length / 2);
-*/
+const mitabla = document.getElementById("tabla-numeros");
+const tablabody = document.getElementById("tabla-body");
+const camponumero = document.getElementById("numero");
+const listanumeros = new Array();
+
 function calcularMediaAritmetica(lista) {
   const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
     return valorAcumulado + nuevoElemento;
@@ -35,4 +33,34 @@ function calcularMediana(lista = []) {
     return `La mediana es: ${mediana}`;
   } else {
   }
+}
+const agregarNumero = (numero = 0 || numero) => {
+  if (typeof numero !== "number")
+    return alert(`El dato ingresado: ${numero} debe ser un número`);
+  if (typeof numero === undefined) return alert("No ha ingresado ningún dato");
+  listanumeros.push(numero);
+  agregarCelda(numero);
+  return listanumeros;
+};
+const agregarCelda = (numero = 0 || numero) => {
+  let fila = document.createElement("tr");
+  let dato = document.createElement("td");
+  dato.appendChild(document.createTextNode(numero));
+  fila.appendChild(dato);
+  tablabody.appendChild(fila);
+};
+function agregarNumeroBtn() {
+  let numleido = parseFloat(document.getElementById("numero").value);
+  if (isNaN(numleido)) {
+    camponumero.value = "";
+    return alert("El dato debe ser un número.");
+  } else {
+    agregarNumero(numleido);
+    camponumero.value = "";
+  }
+}
+function medianaBoton() {
+  let mediana = calcularMediana(listanumeros);
+  let resultado = document.getElementById("resultado");
+  resultado.textContent = `La Mediana es: ${mediana}`;
 }
--- taller3/promedio.js
+++ taller3/promedio.js
@@ -1,47 +1,44 @@
-const lista1 = [100, 200, 300, 500];
-const mitabla=document.getElementById("tabla-numeros")
-const tablabody=document.getElementById("tabla-body");
-const camponumero=document.getElementById("numero");
-//const numleido=parseFloat(document.getElementById("numero").value);
-function calcularMediaAritmetica(lista) {
-  /* let sumaLista = 0;
+//const lista1 = [100, 200, 300, 500];
+const mitabla = document.getElementById("tabla-numeros");
+const tablabody = document.getElementById("tabla-body");
+const camponumero = document.getElementById("numero");
+const listanumeros = new Array();
 
-  for (let i = 0; i < lista.length; i++) {
-    sumaLista += lista[i];
-  } */
+function calcularMediaAritmetica(lista) {
   const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
     return valorAcumulado + nuevoElemento;
   });
   const promedioLista = sumaLista / lista.length;
   return promedioLista;
 }
-const listanumeros= new Array();
-const agregarNumero = (numero=0 ||numero) =>{
-  if(typeof numero!== "number") return alert(`El dato ingresado: ${numero} debe ser un número`);
-  if(typeof numero=== undefined) return alert("No ha ingresado ningún dato"); 
-    listanumeros.push(numero);
-    agregarCelda(numero);
-    return listanumeros;
-}
-const agregarCelda= (numero=0||numero) => {
-  let fila=document.createElement("tr");
-  let dato=document.createElement("td");
+
+const agregarNumero = (numero = 0 || numero) => {
+  if (typeof numero !== "number")
+    return alert(`El dato ingresado: ${numero} debe ser un número`);
+  if (typeof numero === undefined) return alert("No ha ingresado ningún dato");
+  listanumeros.push(numero);
+  agregarCelda(numero);
+  return listanumeros;
+};
+const agregarCelda = (numero = 0 || numero) => {
+  let fila = document.createElement("tr");
+  let dato = document.createElement("td");
   dato.appendChild(document.createTextNode(numero));
   fila.appendChild(dato);
   tablabody.appendChild(fila);
-}
-function agregarNumeroBtn(){
-  let numleido=parseFloat(document.getElementById("numero").value);
+};
+function agregarNumeroBtn() {
+  let numleido = parseFloat(document.getElementById("numero").value);
   if (isNaN(numleido)) {
     camponumero.value = "";
     return alert("El dato debe ser un número.");
-  }else{
-  agregarNumero(numleido);
-  camponumero.value = "";
+  } else {
+    agregarNumero(numleido);
+    camponumero.value = "";
   }
 }
-function promedioBoton(){
-  let promedio=calcularMediaAritmetica(listanumeros);
-  let resultado= document.getElementById("resultado");
-  resultado.textContent =`El promedio es: ${promedio}`;
+function promedioBoton() {
+  let promedio = calcularMediaAritmetica(listanumeros);
+  let resultado = document.getElementById("resultado");
+  resultado.textContent = `El promedio es: ${promedio}`;
 }
