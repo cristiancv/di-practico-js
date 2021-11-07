712a31512f53e0a1880349065f6c1b026214bd79
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2021-11-07 15:05:14 -0500

    Taller 4 salarios archivo html listo

--- taller4/analisis.js
+++ taller4/analisis.js
@@ -38,3 +38,37 @@ const salariosEcuTop10 = salariosEcuSorted.splice(spliceStart, spliceCount);
 
 const medianaEcuTop10 = medianaSalarios(salariosEcuTop10);
 console.log(medianaGeneralEcu, medianaEcuTop10);
+
+//Funciones para llenar las celdas
+const agregarNumero = (numero = 0 || numero) => {
+  if (typeof numero !== "number")
+    return alert(`El dato ingresado: ${numero} debe ser un número`);
+  if (typeof numero === undefined) return alert("No ha ingresado ningún dato");
+  listanumeros.push(numero);
+  agregarCelda(numero);
+  return listanumeros;
+};
+const agregarCelda = (numero = 0 || numero) => {
+  let fila = document.createElement("tr");
+  let dato = document.createElement("td");
+  dato.appendChild(document.createTextNode(numero));
+  fila.appendChild(dato);
+  tablabody.appendChild(fila);
+};
+function agregarNumeroBtn() {
+  let numleido = parseFloat(document.getElementById("numero").value);
+  if (isNaN(numleido)) {
+    camponumero.value = "";
+    return alert("El dato debe ser un número.");
+  } else {
+    agregarNumero(numleido);
+    camponumero.value = "";
+  }
+}
+function modaBoton() {
+  let moda = calcularModa(listanumeros);
+  let resultado = document.getElementById("resultado");
+  let num = moda[0];
+  let veces = moda[1];
+  resultado.textContent = `La Moda es: ${num}, ${veces} veces.`;
+}
